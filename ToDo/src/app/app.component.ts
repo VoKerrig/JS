51d05d078f706d35cacb3684b1b1b4306887b33c
VoKerrig <vokerrig@gmail.com>
2022-07-28 19:01:35 +0300

    minor_change

--- ToDo/src/app/app.component.ts
+++ ToDo/src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'ToDo';
+
+  allItems = [
+    { description: ''}
+  ];
+
+  get items() {
+      return this.allItems;
+    }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description
+    });
+  }
 }
